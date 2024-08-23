2a605b934ef96038ffd4ba25b3ca4645ae8b784c
mdsaiful170 <saifulsylhet00@gmail.com>
2024-08-24 00:14:08 +0600

    upadateto

--- main.js
+++ main.js
@@ -7,35 +7,36 @@ function displayData() {
       return `<div class="milsesonte p-3 m-3 border-b border-slate-700 text-base" id="${
         res._id
       }">
-              <div class="flex">
-                <div class="w-5">
-                  <input
-                  type="checkbox"
-                  onclick="checkboxdata(this,${res._id})"
-                  class="mr-[5px] align-middle"
-                  name=""
-                  id=""
-                />
-                </div>
-                <p class="cursor-pointer" onclick="datalist(this, ${res._id})">
-                  ${res.name}
-                  <span><i class="ri-arrow-down-s-line"></i></span>
-                </p>
-              </div>
+        <div class="flex">
+          <div class="w-5">
+            <input
+            type="checkbox"
+            onclick="checkboxdata(this,${res._id})"
+            class="mr-[5px] align-middle"
+            name=""
+            id=""
+          />
+          </div>
+          <p class="cursor-pointer" onclick="datalist(this, ${res._id})">
+            ${res.name}
+            <span><i class="ri-arrow-down-s-line"></i></span>
+          </p>
+        </div>
 
-              <div class="overflow-hidden max-h-0">
-                ${res.modules
-                  .map((list) => {
-                    return `<div class="border-b border-slate-700 p-3 m-3 text-base ">
-                  <p>${list.name}</p>
-                </div>`;
-                  })
-                  .join("")}
-              </div>
-            </div>`;
+        <div class="overflow-hidden max-h-0">
+          ${res.modules
+            .map((list) => {
+              return `<div class="border-b border-slate-700 p-3 m-3 text-base ">
+            <p>${list.name}</p>
+          </div>`;
+            })
+            .join("")}
+        </div>
+      </div>`;
     })
     .join(" ")}`;
 }
+
 
 function datalist(result, num) {
   const dataitems = result.parentNode.parentNode.children[1];
@@ -61,8 +62,8 @@ function showImageBox(num) {
   const imgbox = document.querySelector(".milseStoneImage");
   const title = document.querySelector(".taitle");
   const description = document.querySelector(".description");
-  title.innerText = mileStoneData[num].name
-  description.innerText = mileStoneData[num].description
+  title.innerText = mileStoneData[num].name;
+  description.innerText = mileStoneData[num].description;
   imgbox.classList.add("opacity-0");
   imgbox.src = mileStoneData[num].image;
 }
@@ -71,18 +72,17 @@ imgbox.onload = function () {
   this.classList.add("opacity-100");
 };
 
-function checkboxdata(sendbox , id){
-  const nodeList = document.querySelector('.nodeList');
-  const milestoneBox = document.querySelector('.milesontMainBOx');
+function checkboxdata(sendbox, id) {
+  const nodeList = document.querySelector(".nodeList");
+  const milestoneBox = document.querySelector(".milesontMainBOx");
   const listId = document.getElementById(id);
-  if(sendbox.checked){
-    nodeList.appendChild(listId)
+  if (sendbox.checked) {
+    nodeList.appendChild(listId);
     milestoneBox.removeChild(listId);
-  }else{
+  } else {
     nodeList.removeChild(listId);
-    milestoneBox.appendChild(listId)
+    milestoneBox.appendChild(listId);
   }
-
 }
 
 displayData();
